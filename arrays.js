719a8b1eaee0d5d1504cac3013985a6393edda7a
kiran225007@gmail.com <kiran225007@gmail.com>
2018-08-10 13:09:09 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(chocolateBars,element)
+{
+  const arr = [element,...chocolateBars]
+  return arr
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars,element)
+{
+  chocolateBars.unshift(element)
+}
+function addElementToEndOfArray(chocolateBars,element)
+{
+  const arr = [...chocolateBars,element]
+  return arr
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars,element)
+{
+  chocolateBars.push(element)
+}
