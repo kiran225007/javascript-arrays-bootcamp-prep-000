bb5ee094c4b0a0b22667d5fa06946bc263357d1c
kiran225007@gmail.com <kiran225007@gmail.com>
2018-08-10 13:42:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 function addElementToBeginningOfArray(array,element)
 {
-  const arr = [...chocolateBars,element]
+  const arr = [element,...chocolateBars]
   return arr
 }
 
